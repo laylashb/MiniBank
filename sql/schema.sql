95ed29d33ca273f250bc103b70d8d84c9ff3a6ac
layla <shabilayla2@email.com>
2026-04-02 18:38:11 +0200

    bonne version confirmée par le prof

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,39 +1,35 @@
-DROP TABLE IF EXISTS clients;
-DROP TABLE IF EXISTS comptes;
-DROP TABLE IF EXISTS transactions;
-
-
-
+-- Table clients
 CREATE TABLE clients (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) NOT NULL,
-  `prenom` varchar(100) NOT NULL,
-  `email` varchar(150) NOT NULL UNIQUE,
-  `ville` varchar(100) DEFAULT NULL,
-  `date_creation` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    ville VARCHAR(100) DEFAULT NULL,
+    date_creation TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE KEY email (email)
+);
 
-
+-- Table comptes
 CREATE TABLE comptes (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `numero_compte` varchar(20) NOT NULL,
-  `solde` decimal(10,2) DEFAULT '0.00',
-  `client_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `numero_compte` (`numero_compte`),
-  KEY `fk_client` (`client_id`),
-  CONSTRAINT `fk_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    id INT NOT NULL AUTO_INCREMENT,
+    numero_compte VARCHAR(20) NOT NULL,
+    solde DECIMAL(10,2) DEFAULT '0.00',
+    client_id INT DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY numero_compte (numero_compte),
+    KEY fk_client (client_id),
+    CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
+);
 
-
+-- Table transactions
 CREATE TABLE transactions (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` enum('depot','retrait') NOT NULL,
-  `montant` decimal(10,2) NOT NULL,
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `compte_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_compte` (`compte_id`),
-  CONSTRAINT `fk_compte` FOREIGN KEY (`compte_id`) REFERENCES `comptes` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    id INT NOT NULL AUTO_INCREMENT,
+    type ENUM('depot','retrait') NOT NULL,
+    montant DECIMAL(10,2) NOT NULL,
+    date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    compte_id INT DEFAULT NULL,
+    PRIMARY KEY (id),
+    KEY fk_compte (compte_id),
+    CONSTRAINT fk_compte FOREIGN KEY (compte_id) REFERENCES comptes (id) ON DELETE CASCADE
+);
